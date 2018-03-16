8b3d622b4f920cb36819dbfb61298cbfa80edfb5
José Jeria <jose.jeria@gmai.com>
2018-03-16 18:23:31 +0100

    Added MVVM-C version of project

---
+++ NewsReader-mvvm-c/NewsReader/GUI/View Controllers/News Overview/ArticlesListViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ArticlesListViewModel.swift
+//  NewsReader
+//
+//  Created by Jose Jeria on 16.03.18.
+//  Copyright © 2018 José Jeria. All rights reserved.
+//
+
+import Foundation
+
+class ArticlesListViewModel {
+    
+    
+    
+}
